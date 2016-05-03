6355dde2e73bbb103eea233b0707920141ae8ac7
Rob Winch <rwinch@gopivotal.com>
2016-05-03 10:04:53 -0500

    Lambda RequestMatchResolver

--- src/main/java/com/example/SecurityConfig.java
+++ src/main/java/com/example/SecurityConfig.java
@@ -1,18 +1,31 @@
 package com.example;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.support.RequestMatchInfo;
+import org.springframework.web.servlet.support.RequestMatchResolver;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	RequestMatchResolver mvcMatcher;
+
+	@Autowired
+	public void setHandlerMappings(List<HandlerMapping> handlerMappings) {
+		mvcMatcher = new RequestMatchResolver();
+		mvcMatcher.setHandlerMappings(handlerMappings);
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
 			.authorizeRequests()
-				.antMatchers("/denyAll").denyAll()
+				.requestMatchers(request -> mvcMatcher.resolve(request).match(request, new RequestMatchInfo("/denyAll"))).denyAll()
 				.and()
 			.httpBasic();
 	}
